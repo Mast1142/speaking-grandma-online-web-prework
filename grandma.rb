55ac2bb13d9244fd01692155f981b4079fbb400e
Samuel W Tong <ssamt1142@gmail.com>
2019-02-01 21:01:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(talk)
+  if #{talk} !!match(/\p{Upper})
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif #{talk}.upcase = true
+    puts "NO, NOT SINCE 1938!"
+  else #{talk} != #{talk}.upcase
+    puts "HUH?! SPEAK UP< SONNY!"
+  end
+end
+
+    
+  
