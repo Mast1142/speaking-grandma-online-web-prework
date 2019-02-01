e08d05626401fd95fa6e623a48ef29f6c35ff2cf
Samuel W Tong <ssamt1142@gmail.com>
2019-02-01 21:54:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,7 @@ def speak_to_grandma(talk)
     return "HUH?! SPEAK UP, SONNY!"
   elsif "#{talk}" == talk.upcase
     puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else "#{talk}" == "I LOVE YOU, GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
